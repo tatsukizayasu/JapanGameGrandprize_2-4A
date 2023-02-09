dae2563cf9832647cb0c95853d6dc32386481da5
hikki1123 <106945420+hikki1123@users.noreply.github.com>
2023-02-09 15:22:39 +0900

    未完成部分の網掛け

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/EfectBeam.h
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/EfectBeam.h
@@ -1,20 +1,20 @@
-#pragma once
-#include "Define.h"
-
-#define RADIUS_MAX 20
-
-class EfectBeam
-{
-private:
-	Location location[3];
-	int shoot_count;
-	int radius[3];
-	float add_loc_y[3];
-public:
-	EfectBeam();
-	~EfectBeam() {};
-
-	void Draw() const;
-	void Update(float,float);
-};
+//#pragma once
+//#include "Define.h"
+//
+//#define RADIUS_MAX 20
+//
+//class EfectBeam
+//{
+//private:
+//	Location location[3];
+//	int shoot_count;
+//	int radius[3];
+//	float add_loc_y[3];
+//public:
+//	EfectBeam();
+//	~EfectBeam() {};
+//
+//	void Draw() const;
+//	void Update(float,float);
+//};
 
