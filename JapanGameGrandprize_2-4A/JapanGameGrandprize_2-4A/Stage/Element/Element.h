219d1bc510ff079075a935e5cbfd20f74fab224e
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-03-03 14:48:20 +0900

    Add Class Element_Wooden_Floor

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Stage/Element/Element.h
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Stage/Element/Element.h
@@ -2,7 +2,23 @@
 #include "../Stage.h"
 
 class Stage_Element :
-    public Stage
+    public Stage, public BoxCollider
 {
+private:
 
+
+protected:
+
+
+public:
+	//コンストラクタ
+	Stage_Element();
+	//コンストラクタ
+	Stage_Element(const int* p_image, Location location, Area area);
+	//デストラクタ
+	~Stage_Element();
+	//更新
+	virtual void Update(Player* player) = 0;
+	//描画
+	virtual void Draw() const = 0;
 };
