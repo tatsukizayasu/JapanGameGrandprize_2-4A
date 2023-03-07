0e5c9731439c4be27ce61992ba5757450b37a120
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-03-07 14:06:39 +0900

    Organize Element file hierarchy

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Stage/Element/Stage_Element.h
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Stage/Element/Stage_Element.h
@@ -1,6 +1,23 @@
 #pragma once
-#include "../Stage.h"
-class Stage_Element :
-    public Stage
+#include "../Stage/Stage.h"
+
+class Stage_Element
 {
+private:
+
+protected:
+
+
+public:
+	
+	//コンストラクタ
+	Stage_Element();
+	//コンストラクタ
+	//Stage_Element(const int* p_image, Location location, Area area);
+	//デストラクタ
+	~Stage_Element();
+	//更新
+	//virtual void Update(Player* player) = 0;
+	//描画
+	//virtual void Draw() const = 0;
 };
