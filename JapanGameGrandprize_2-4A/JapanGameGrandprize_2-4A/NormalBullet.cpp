7fc36223b673b61d5c0845d29336d369b7025098
ryusuke-oshiro <106945398+ryusuke-oshiro@users.noreply.github.com>
2023-04-03 19:06:51 +0900

    04/03

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/NormalBullet.cpp
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/NormalBullet.cpp
@@ -8,7 +8,8 @@
 //-----------------------------------
 NormalBullet::NormalBullet()
 {
-
+	Tick = 0;
+	Explosion[0].image = LoadGraph("Images/Player/img01.png");
 	location.x = 0.0;
 	location.y = 0.0;
 	radius = 4;
@@ -34,7 +35,7 @@ NormalBullet::NormalBullet()
 NormalBullet::NormalBullet(float player_x, float player_y,
 	                         bool direction,ChemicalFormulaParameter* chemical)
 {
-
+	Tick = 0;
 	delete_flg = false;
 	scrool_x = CameraWork::GetCamera().x;
 	scrool_y = CameraWork::GetCamera().y;
@@ -48,6 +49,25 @@ NormalBullet::NormalBullet(float player_x, float player_y,
 	attribute = chemical->atribute;
 	this->player_state = player_state;
 	player_direction = direction;
+
+	switch (attribute)
+	{
+	case ATTRIBUTE::NORMAL:
+		break;
+	case ATTRIBUTE::EXPLOSION:
+		Explosion[0].image = LoadGraph("Images/Player/img01.png");
+		Explosion[1].x = player_x;
+		Explosion[1].y = player_y;
+		Explosion[1].X_radius = 5;
+		Explosion[1].Y_radius = 10;
+		break;
+	case ATTRIBUTE::MELT:
+		break;
+	case ATTRIBUTE::PARALYSIS:
+		break;
+	case ATTRIBUTE::POISON:
+		break;
+	}
 
 	efect_count = 0;
 	delete_flg = false;
@@ -90,7 +110,6 @@ void NormalBullet::Draw() const
 		}
 		else
 		{
-			/*Explosion(location.x - scrool_x, location.y - scrool_y);*/
 			DrawBox(location.x - scrool_x, location.y - scrool_y, (location.x - scrool_x) + 20, (location.y - scrool_y) + 10, GLAY, TRUE);
 		}
 		break;
@@ -107,7 +126,38 @@ void NormalBullet::Draw() const
 		}
 		else
 		{
-			DrawBox(location.x - scrool_x, location.y - scrool_y, (location.x - scrool_x) + 20, (location.y - scrool_y) + 10, ORANGE, TRUE);
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+			SetDrawBlendMode(DX_BLENDMODE_ADD, 255);
+			for (int i = 0; i < 5; i++) 
+			{
+				DrawRotaGraph(location.x - scrool_x, location.y - scrool_y, 1, 1, Explosion[0].image, TRUE, FALSE);
+			}
+			if (Explosion[1].display_permit == TRUE && Explosion[1].BrendMode_ALPFA > 0) 
+			{
+				SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+				SetDrawBlendMode(DX_BLENDMODE_ALPHA, Explosion[1].BrendMode_ALPFA);
+				DrawOvalAA(Explosion[1].x, Explosion[1].y, Explosion[1].X_radius - 10, Explosion[1].Y_radius - 10, 20, 0xff0000, FALSE, 10);
+				DrawOvalAA(Explosion[1].x, Explosion[1].y, Explosion[1].X_radius, Explosion[1].Y_radius, 20, 0xff0000, FALSE, 6);
+			}
+			for (int i = 0; i < PARTICLE; i++) 
+			{
+				if (Explosion[2].display_permit_Array[i] == TRUE) 
+				{
+					SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+					SetDrawBlendMode(DX_BLENDMODE_ADD, Explosion[2].BrendMode_ALPFA_Array[i]);
+					DrawOvalAA(Explosion[2].x_Array[i] + 10, Explosion[2].y_Array[i], 8, Explosion[2].OvalY_Array_radius[i], 20, 0xff0000, FALSE, 3.0f);
+				}
+			}
+			for (int i = 0; i < PARTICLE; i++) 
+			{
+				if (Explosion[3].display_permit_Array[i] == TRUE) 
+				{
+					SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+					SetDrawBlendMode(DX_BLENDMODE_ALPHA, Explosion[3].BrendMode_ALPFA_Array[i]);
+					DrawRotaGraph(Explosion[3].x_Array[i] - 10, Explosion[3].y_Array[i], 1.5, 1, Explosion[0].image, true, false);
+				}
+			}
+			//DrawBox(location.x - scrool_x, location.y - scrool_y, (location.x - scrool_x) + 20, (location.y - scrool_y) + 10, ORANGE, TRUE);
 		}
 		break;
 	case ATTRIBUTE::MELT:
@@ -168,7 +218,7 @@ void NormalBullet::Draw() const
 //-----------------------------------
 void NormalBullet::Update(const Stage* stage_pointa)
 {
-
+	Tick++;
 	float scrool_x = CameraWork::GetCamera().x;
 	if (!player_direction)
 	{
@@ -181,6 +231,17 @@ void NormalBullet::Update(const Stage* stage_pointa)
 				break;
 			case ATTRIBUTE::EXPLOSION:
 				location.x += 5;
+				if (Tick == 1) 
+				{
+					if (Explosion[1].display_permit == FALSE) 
+					{
+						Explosion[1].x = location.x;
+						Explosion[1].y = location.y;
+						Explosion[1].X_radius = 5;
+						Explosion[1].Y_radius = 10;
+						Explosion[1].display_permit = TRUE;
+					}
+				}
 				break;
 			case ATTRIBUTE::MELT:
 				location.x += 2;
@@ -214,8 +275,26 @@ void NormalBullet::Update(const Stage* stage_pointa)
 	{
 		if (!HitBlock(stage_pointa) && location.x - scrool_x > 0 && !delete_flg)//弾の座標更新(左
 		{
+			switch (attribute)
+			{
+			case ATTRIBUTE::NORMAL:
+				location.x -= BULLET_SPEED;
+				break;
+			case ATTRIBUTE::EXPLOSION:
+				location.x -= 5;
+				break;
+			case ATTRIBUTE::MELT:
+				location.x -= 2;
+				break;
+			case ATTRIBUTE::PARALYSIS:
+				location.x -= 2;
+				break;
+			case ATTRIBUTE::POISON:
+				location.x -= 2;
+				break;
+			}
 
-			location.x -= BULLET_SPEED;
+			//location.x -= BULLET_SPEED;
 		
 		}
 		else                   //着弾時のエフェクトの座標更新(左
