535a388c356bf34043aa43cbe3453e26d788d74a
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2023-04-11 14:53:52 +0900

    Add Element_Item_Drop_Object Class

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/ItemController.h
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/ItemController.h
@@ -1,5 +1,7 @@
 #pragma once
 #include"Item.h"
+#include "ElementItem.h"
+#include <vector>
 
 class ItemController
 {
@@ -24,6 +26,8 @@ public:
 
 	//アイテムの生成
 	void SpawnItem(const class EnemyBase* enemy);
+
+	void Stage_SpawnItem(int drop_volume, int dorp_type_volume, Location location, std::vector<ElementItem>element_item);
 
 	//アイテムの並び替え
 	void SortItem(const int item_num);
