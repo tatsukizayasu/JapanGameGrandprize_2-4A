03bfff948859759583cd7d9fe7457579e4237e19
Folivora-0818 <106945429+Folivora-0818@users.noreply.github.com>
2023-04-14 11:58:20 +0900

    ラスボス

---
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/LastBoss.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "EnemyBase.h"
+class LastBoss :
+    public EnemyBase
+{
+private:
+
+    bool attack; //攻撃が当たったか
+    int damage; //ダメージ
+    int attack_interval; //次の攻撃までの時間
+    int animation; //アニメーション
+    int image_argument; //画像の引数
+    int attack_time; //攻撃している時間(デバッグ用)
+private:
+
+    //移動時のアニメーション
+    void MoveAnimation();
+public:
+
+    //コンストラクタ
+    LastBoss(Location);
+
+    //デストラクタ
+    ~LastBoss();
+
+    //更新
+    void Update(const class Player* player, const class Stage* stage) override;
+
+    //アイドル状態
+    void Idol() override;
+
+    //移動
+    void Move(const Location player_location) override;
+
+    //落下
+    void Fall() override;
+
+    //攻撃
+    void  Attack(const Location) override;
+
+    //攻撃が当たっているか
+    AttackResource Hit() override;
+
+    //死亡
+    void Death() override;
+
+    //プレイヤーの弾との当たり判定
+    void HitBullet(const BulletBase* bullet) override;
+
+    //描画
+    void Draw() const override;
+
+    //座標の取得
+    Location GetLocation() const override;
+
+#ifdef _DEBUG
+    //更新(DotByDot)
+    void Update(const ENEMY_STATE state) override;
+
+    //描画(DotByDot)
+    void DebugDraw() override;
+#endif //_DEBUG
+};
+
