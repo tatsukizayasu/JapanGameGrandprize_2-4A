1ec1efe6ff272e405bb519600b6d49d2c6c5a82b
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-05-11 11:42:34 +0900

    ドラゴンのバグ修正
    あと画像追加

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Dragon.h
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Dragon.h
@@ -24,6 +24,7 @@ class Dragon :public EnemyBase
 private:
 	int image;
 	int animation; //画像のアニメーション
+	int fly_image[2]; //飛ぶ画像
 	int magic_num; //遠距離攻撃を使った回数
 	int attack_method; //攻撃方法
 	int switchover_time; //攻撃切り替え時間
