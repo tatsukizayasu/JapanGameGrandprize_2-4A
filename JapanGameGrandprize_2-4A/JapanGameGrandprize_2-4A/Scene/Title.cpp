03f5a4bde028572ec6177933a6fd859a290662c6
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2023-05-17 12:11:47 +0900

    fixed default element of player

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Scene/Title.cpp
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Scene/Title.cpp
@@ -132,7 +132,7 @@ AbstractScene* Title::Update()
 		input_margin = 0;
 		MENU current_selection = static_cast<MENU>(select_menu);
 
-		unsigned int element_volume[PLAYER_ELEMENT] = { 30,30,30,30,30,30,30 };
+		unsigned int element_volume[PLAYER_ELEMENT] = { 10,10,10,10,10,10,10 };
 		
 		switch (current_selection)
 		{
