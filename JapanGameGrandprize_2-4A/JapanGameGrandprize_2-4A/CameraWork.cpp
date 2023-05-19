627b61e9c04790e9b5f796847a6ba811e92a5471
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2023-05-19 13:44:05 +0900

    Update Stage04

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/CameraWork.cpp
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/CameraWork.cpp
@@ -64,7 +64,15 @@ CameraWork::CameraWork(float camera_x, float camera_y, Player* player, Stage* st
 	player_dir = true;
 	player_dir_y = false;
 
-	is_lock = false;
+	if (stage_num != 4)
+	{
+		is_lock = false;
+	}
+	else
+	{
+		is_lock = true;
+	}
+	
 
 }
 
@@ -81,7 +89,7 @@ CameraWork::~CameraWork()
 //-----------------------------------
 void CameraWork::Update()
 {
-	if (is_lock == true) { return; }
+	if (is_lock == true || stage->GetStageNum() == 4) { return; }
 	//printfDx("state:%d\n", state);
 	
 	//clsDx();
