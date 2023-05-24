22809347a7a629cc4e488f1f6bac20d3f7cd1fc0
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2023-05-24 14:11:28 +0900

    トレント登場方法変更

--- JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Torrent.cpp
+++ JapanGameGrandprize_2-4A/JapanGameGrandprize_2-4A/Torrent.cpp
@@ -661,23 +661,30 @@ void Torrent::Draw() const
 
 	int num = static_cast<int>(kind) - static_cast<int>(ENEMY_KIND::SLIME);
 
-	switch (attack_state)
+	if (state == ENEMY_STATE::MOVE)
 	{
-	case TORRENT_ATTACK::TACKLE:
 		DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][image_argument % 8], TRUE, !left_move);
-		break;
-	case TORRENT_ATTACK::LEAF_CUTTER:
-		DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][image_argument % 10 + 17], TRUE, !left_move);
-		break;
-	case TORRENT_ATTACK::DROP_NUTS:
-		DrawRotaGraph3(SCREEN_WIDTH / 2, 50, 640, 640, 1, 0.05, 0, magic_circle_image, TRUE);
-		DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][image_argument % 9 + 8], TRUE, !left_move);
-		break;
-	case TORRENT_ATTACK::NONE:
-		DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][0], TRUE, !left_move);
-		break;
-	default:
-		break;
+	}
+	else
+	{
+		switch (attack_state)
+		{
+		case TORRENT_ATTACK::TACKLE:
+			DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][image_argument % 8], TRUE, !left_move);
+			break;
+		case TORRENT_ATTACK::LEAF_CUTTER:
+			DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][image_argument % 10 + 17], TRUE, !left_move);
+			break;
+		case TORRENT_ATTACK::DROP_NUTS:
+			DrawRotaGraph3(SCREEN_WIDTH / 2, 50, 640, 640, 1, 0.05, 0, magic_circle_image, TRUE);
+			DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][image_argument % 9 + 8], TRUE, !left_move);
+			break;
+		case TORRENT_ATTACK::NONE:
+			DrawRotaGraphF(draw_location.x, draw_location.y, 0.7, 0, images[num][0], TRUE, !left_move);
+			break;
+		default:
+			break;
+		}
 	}
 
 	if (state != ENEMY_STATE::DEATH)
